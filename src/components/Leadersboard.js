c16b92ff0c202264154d157de24fae6db8886e01
mwaqasafzal <mwaqas7064@gmail.com>
2020-11-16 18:58:38 +0500

    style:adjusted styles of all views

--- src/components/Leadersboard.js
+++ src/components/Leadersboard.js
@@ -1,40 +1,39 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import UserStats from './UserStats'
+import React from "react";
+import { connect } from "react-redux";
+import UserStats from "./UserStats";
 
-const Leaderboard = props => {
-
-    return (
-        <div className="leadersboard">
-            <h3 className="center title">Leadersboard</h3>
-            <table className="leadersboard-rating">
-                <tbody>
-                    {props.users.map(user => (
-                        <UserStats
-                            key={user.id}
-                            name={user.name}
-                            avatar={user.avatarURL}
-                            asked={user.questions}
-                            answered={user.answers}
-                            total={user.questions + user.answers}
-                        />))}
-                </tbody>
-            </table>
-        </div>
-
-    );
-}
+const Leaderboard = (props) => {
+  return (
+    <div className="leadersboard">
+      <h3 className="center title">Leadersboard</h3>
+      <div className="leadersboard-rating">
+        <table>
+          <tbody>
+            {props.users.map((user) => (
+              <UserStats
+                key={user.id}
+                name={user.name}
+                avatar={user.avatarURL}
+                asked={user.questions}
+                answered={user.answers}
+                total={user.questions + user.answers}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 const mapStateToProps = ({ users }) => {
-
-    return {
-        users: Object.values(users)
-            .map(user => ({
-                ...user,
-                answers: Object.keys(user.answers).length,//we have an object of answers 
-                questions: user.questions.length
-            }))
-            .sort((a, b) => (b.answers + b.questions) - (a.answers + a.questions))
-    }
-
-}
+  return {
+    users: Object.values(users)
+      .map((user) => ({
+        ...user,
+        answers: Object.keys(user.answers).length, //we have an object of answers
+        questions: user.questions.length,
+      }))
+      .sort((a, b) => b.answers + b.questions - (a.answers + a.questions)),
+  };
+};
 export default connect(mapStateToProps)(Leaderboard);
